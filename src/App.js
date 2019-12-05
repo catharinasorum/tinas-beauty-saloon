993998a611e1d4f8c0743ed486edca737511596f
Catharina <catharina.sorum@outlook.com>
2019-12-05 15:52:34 +0100

    txt

--- src/App.js
+++ src/App.js
@@ -13,6 +13,38 @@ const App = () => {
           < FaShoppingBasket className='menu-basket'/>
         </nav>
       </section>
+      
+      <section className='Main'>
+        <section className='background p1'>
+          <h2>VOTED BEST HAIR SALON IN ARMADILLO</h2>
+        </section>
+
+        <section className='text home'>
+          <p>Our award-winning salon (or saloon as we call it) is a full-service salon committed to giving guests the best salon experiens in the wild west. We serve whiskey and beer while you're here.
+          <br/>
+          <br/>
+          Hitch your horses right outside! We have a stable boy who will take care of your horse free of charge.
+          <br/>
+          <br/>
+          Sadly, we cannot take care of your cows, they are to many for us to handle.</p>
+        </section>
+
+        <section className='background p2'>
+          <h2>Services</h2>
+        </section>
+
+        <section className='text services'>
+          <p>We can offer these services</p>
+        </section>
+
+        <section className='Footer'>
+            <h2>Get in touch with us!</h2>
+            <p><b>Phone number:</b> +1-602-555-0162</p>
+            <p><b>Mail address:</b> post@tinasaloon.com</p>
+            <p><b>Address:</b> 1374 Boone Crocket Lane, Armadillo</p>
+        </section>
+
+      </section>
     </div>
   );
 }
