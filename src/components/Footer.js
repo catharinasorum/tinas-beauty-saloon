43fffce773b725aedc172742f358cd2ab7a33e8a
Catharina <catharina.sorum@outlook.com>
2019-12-10 09:37:26 +0100

    added css for the footer and the whole page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,19 +9,21 @@ const Footer = () => {
     const [employees] = useState(employeeFiles.persons)
 
     return (
-        <section className='Footer'>
-            <h2>Get in touch with us!</h2>
-                <p><b>Phone number:</b> +1-602-555-0162</p>
-                <p><b>Mail address:</b> post@tinasaloon.com</p>
-                <p><b>Address:</b> 1374 Boone Crocket Lane, Armadillo</p>
+        <section className='Footer text'>
             <h2>Here are the amazing workers at Tina's Beauty Saloon</h2>
                 <div className="employees">
                     {
                         employees.map (
                             (employee, i) => <FooterEmployees name={employee.name} key={i} number={i} picture={employee.picture} position={employee.position} />
-                        )
-                    }
+                            )
+                        }
                 </div>
+            <h2>Get in touch with us!</h2>
+                <p><b>Phone number:</b> +1-602-555-0162</p>
+                <p><b>Mail address:</b> post@tinasaloon.com</p>
+                <p><b>Address:</b> 1374 Boone Crocket Lane, Armadillo</p>
+
+            
         </section>
     )
 }
